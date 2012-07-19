02e8ea473724e30ba7f3c4d8658d2671a90f0b3b
Fadion Dashi <jonidashi@gmail.com>
2012-07-19 18:27:02 +0200

    Gizmo: Changed short-tags to normal PHP tags
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- psyche/core/debug/gizmo/assets/index.php
+++ psyche/core/debug/gizmo/assets/index.php
@@ -1,14 +1,14 @@
 <?= str_repeat("\n", 50); ?>
-<script type="text/javascript"><?= file_get_contents(__DIR__.'/jquery.js'); ?></script>
-<script type="text/javascript"><?= file_get_contents(__DIR__.'/scripts.js'); ?></script>
-<style type="text/css"><?= file_get_contents(__DIR__.'/styles.css'); ?></style>
+<script type="text/javascript"><?php echo file_get_contents(__DIR__.'/jquery.js'); ?></script>
+<script type="text/javascript"><?php echo file_get_contents(__DIR__.'/scripts.js'); ?></script>
+<style type="text/css"><?php echo file_get_contents(__DIR__.'/styles.css'); ?></style>
 
-<div id="gizmo-console"><?= $console; ?></div>
+<div id="gizmo-console"><?php echo $console; ?></div>
 <div id="gizmo-toolbar">
 	<div class="gizmo-console">Debug Console</div>
-	<div class="gizmo-clock"><?= $execution_time; ?></div>
-	<div class="gizmo-ram"><?= $memory; ?> RAM</div>
-	<div class="gizmo-queries"><?= $queries; ?> Queries</div>
-	<div class="gizmo-controller"><?= $controller; ?></div>
+	<div class="gizmo-clock"><?php echo $execution_time; ?></div>
+	<div class="gizmo-ram"><?php echo $memory; ?> RAM</div>
+	<div class="gizmo-queries"><?php echo $queries; ?> Queries</div>
+	<div class="gizmo-controller"><?php echo $controller; ?></div>
 	<div class="gizmo-close">Close</div>
 </div>
